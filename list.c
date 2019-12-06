0e5a8e86421a1db84c63adbeda9f43ff94389160
kagong <yun8755975@gmail.com>
2019-12-06 06:56:19 -0800

    add list.h

--- list.c
+++ list.c
@@ -2,18 +2,84 @@
 #include<stdlib.h>
 #include "list.h"
 Nptr make_list(){
-    Nptr temp = (Nptr)malloc(sizeof(Node));
-    temp -> node_ptr = NULL;
-    dist = 0;
-    next = NULL;
-    return temp;
+    Nptr new = (Nptr)malloc(sizeof(Node));
+    new -> data_node = NULL;
+    new -> dist = 0;
+    new -> next = NULL;
+    return new;
 }
-void insert_ordered(Nptr head,void* data_node ,){
+void insert_ordered(Nptr head,void* data_node ,double dist){
     Nptr temp = head;
+    Nptr new = (Nptr)malloc(sizeof(Node));
+    new -> data_node = NULL;
+    new -> dist = dist;
+    new -> next = NULL;
     while(temp -> next != NULL){
-        if(temp -> )
+        if(temp -> dist > dist){
+            new -> next = temp -> next;
+            temp -> next = new;
+            return ;
+        }
         temp = temp->next;
     }
+    temp -> next = new;
 }
-void push(void*);
-Nptr pop();
+Nptr pop(Nptr head){
+    Nptr temp = NULL;
+    if(head -> next == NULL){
+        printf("error! head empty -pop\n");
+        return NULL;
+    }
+    temp = head -> next;
+    head -> next = head -> next -> next;
+    return temp;
+    
+}
+Nptr delete_tail(Nptr head){
+    Nptr temp = NULL;
+    if(head -> next == NULL){
+        printf("error! head empty -delete\n");
+        return NULL;
+    }
+    temp = head ;
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+    return temp;
+}
+double top_dist(Nptr head){
+    Nptr temp = NULL;
+    if(head -> next == NULL){
+        printf("error! head empty -pop\n");
+        return 0;
+    }
+    temp = head -> next;
+    head -> next = head -> next -> next;
+    return temp -> dist;
+
+}
+double tail_dist(Nptr head){
+    Nptr temp = NULL;
+    if(head -> next == NULL){
+        printf("error! head empty -delete\n");
+        return 0;
+    }
+    temp = head;
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+    return temp -> dist;
+}
+void free_list(Nptr head){
+    Nptr temp = NULL;
+    if(!head)
+        return ;
+    temp = head -> next;
+    while(temp){
+        Nptr temp2 = temp -> next;
+        free(temp);
+        temp = temp2;
+    }
+    free(head);
+}
+
--- list.h
+++ list.h
@@ -1,12 +1,14 @@
 #ifndef LIST_H
 #define LIST_H
 typedef struct _node{
-    void* node_ptr;
+    void* data_node;
     double dist;
     struct _node *next;
 }Node,*Nptr;
-Nptr make_list();
-void insert_ordered(void*);
-void push(void*);
-Nptr pop();
+void insert_ordered(Nptr head,void* data_node ,double dist);
+Nptr pop(Nptr head);
+Nptr delete_tail(Nptr head);
+double top_dist(Nptr head);
+double tail_dist(Nptr head);
+void free_list(Nptr head);
 #endif
